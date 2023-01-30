378f3be06bdb7cb86ab083b8a5379f7cf3724914
havardmj <havard.myrvold.johannessen@nav.no>
2023-01-30 06:01:20 +0100

    oppdatert login

--- src/main/java/no/inmeta/orderplacement/controller/ConsultantController.java
+++ src/main/java/no/inmeta/orderplacement/controller/ConsultantController.java
@@ -1,10 +1,12 @@
 package no.inmeta.orderplacement.controller;
 
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import no.inmeta.orderplacement.Consultant.AddConsultant;
 import no.inmeta.orderplacement.dto.ConsultantDto;
+import no.inmeta.orderplacement.dto.LoginService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,11 +20,22 @@ public class ConsultantController {
 
     private final ConsultantDto consultantDto;
 
+    private final LoginService loginService;
+
 
     @Transactional
     @PostMapping("/add-consultant")
     public ResponseEntity<?> addConsultant(@RequestBody AddConsultant addConsultant) {
         consultantDto.addConsultant(addConsultant);
         return new ResponseEntity<>(HttpStatus.valueOf(200));
+    }
+
+    @GetMapping("/login-consultant")
+    public ResponseEntity<?> loginConsultant(
+            @RequestHeader(value = "email-address", required = false) String loginEmail,
+            @RequestHeader(value = "password", required = false) String loginPassword
+    ) throws JsonProcessingException {
+        String json = loginService.loginConsultant(loginEmail, loginPassword);
+        return new ResponseEntity<>(json, HttpStatus.OK);
     }
 }
--- src/main/java/no/inmeta/orderplacement/dto/LoginService.java
+++ src/main/java/no/inmeta/orderplacement/dto/LoginService.java
@@ -1,8 +1,11 @@
 package no.inmeta.orderplacement.dto;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import no.inmeta.orderplacement.Consultant.Consultant;
+import no.inmeta.orderplacement.Consultant.LoginConsultant;
 import no.inmeta.orderplacement.repository.ConsultantRepository;
 import org.springframework.stereotype.Component;
 
@@ -12,6 +15,21 @@ import org.springframework.stereotype.Component;
 public class LoginService {
 
     private final ConsultantRepository consultantRepository;
+
+    private final ObjectMapper objectMapper;
+
+
+    public String loginConsultant(String emailAddress, String consultantPassword) throws JsonProcessingException {
+        Consultant user = findSalesRepresentative(emailAddress, consultantPassword);
+        return objectMapper.writeValueAsString(
+                LoginConsultant.builder()
+                        .id(user.getId())
+                        .firstName(user.getFirstName())
+                        .lastName(user.getLastName())
+                        .emailAddress(user.getEmailAddress())
+                        .phoneNumber(user.getPhoneNumber()).build()
+        );
+    }
 
     public Consultant findSalesRepresentative(String emailAddress, String consultantPassword) {
         Consultant consultant = consultantRepository.findConsultantByEmailAddressAndConsultantPassword(
